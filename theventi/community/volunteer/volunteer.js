27e9b57cde7e796b22c0fe4e60c282021b92dbb6
jbinyim12@naver.com <jbinyim12@naver.com>
2024-02-29 18:17:28 +0900

    volunteer

---
+++ theventi/community/volunteer/volunteer.js
@@ -0,0 +1,11 @@
+// const link = document.querySelectorAll(".volun-bar-link");
+
+// for (let i = 0; i < link.length; i++) {
+//   link[i].addEventListener("click", function () {
+//     // e.preventDefault();
+//     for (let j = 0; j < link.length; j++) {
+//       link[j].classList.remove("active");
+//     }
+//     this.classList.add("active");
+//   });
+// }
