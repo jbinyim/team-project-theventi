dd24175d8674ad99b525c8a588941c185712dca6
jbinyim <jbinyim12@naver.com>
2024-03-03 21:14:37 +0900

    240303 message

--- theventi/community/message/message.js
+++ theventi/community/message/message.js
@@ -2,4 +2,4 @@ const arrow = document.querySelectorAll(".fa-solid");
 const banner = document.querySelector(".message-banner-img");
 const pics = ["sum-1.jpg", "sum-2.jpg", "sum-3.jpg", "sum-4.jpg"];
 
-banner.style.backgroundImage = `url(../../img/04community/05message/${[0]})`;
+// banner.style.backgroundImage = `url(../../img/04community/05message/${[0]})`;
