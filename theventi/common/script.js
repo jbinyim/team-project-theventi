c7104b59cb5e63e78cc0769cc8117932c4191536
JJleem <leemjaejun@gmail.com>
2024-03-06 18:30:27 +0900

    jaejun

--- theventi/common/script.js
+++ theventi/common/script.js
@@ -1,3 +1,6 @@
+//===============================================================
+//header dropdown
+
 const headerList = document.querySelector("#header-nav");
 const header = document.querySelector("header");
 
@@ -5,10 +8,6 @@ console.log(header);
 const dropDown = document.querySelector("#dropdown");
 console.log(dropDown);
 
-// headerList.onclick = function () {
-//   dropDown.classList.toggle("hover");
-//   console.log(dropDown);
-// };
 const opendrop = () => {
   dropDown.style.display = "block";
   body.style.transition = "all 0.5s linear";
@@ -23,10 +22,44 @@ dropDown.onmouseout = closedrop;
 
 console.log(opendrop);
 
-// wrap.addEventListener("mouseover", () => {
-//   logo.classList.add("active");
-// });
+//===============================================================
+// footer popup
 
-// wrap.addEventListener("mouseout", () => {
-//   logo.classList.remove("active");
-// });
+const popup2 = document.querySelector("#popup2");
+
+const popupWidth2 = 600;
+const popupheight2 = 500;
+
+popup2.addEventListener("click", () => {
+  const left = (window.screen.availWidth - popupWidth2) / 2;
+  const top = (window.screen.availHeight - popupheight2) / 2;
+  window.open(
+    "popup2.html",
+    "event",
+    `width=${popupWidth2} height =${popupheight2} left =${left} top=${top}`
+  );
+});
+const popup3 = document.querySelector("#popup3");
+
+const popupWidth3 = 600;
+const popupheight3 = 500;
+
+popup3.addEventListener("click", () => {
+  const left = (window.screen.availWidth - popupWidth3) / 2;
+  const top = (window.screen.availHeight - popupheight3) / 2;
+  window.open(
+    "popup3.html",
+    "event",
+    `width=${popupWidth3} height =${popupheight3} left =${left} top=${top}`
+  );
+});
+
+// const open = window.open();
+
+// open = () => {
+//   const left = (window.screen.availWidth - popupWidth3) / 2;
+//   const top = (window.screen.availHeight - popupheight3) / 2;
+//   "popup2.html",
+//     "event",
+//     `width=${popupWidth3} height =${popupheight3} left =${left} top=${top}`;
+// };
