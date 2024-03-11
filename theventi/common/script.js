e74f853b0113da8f816c06d08851c8f503f18d25
JJleem <leemjaejun@gmail.com>
2024-03-11 18:29:47 +0900

    jaejun

--- theventi/common/script.js
+++ theventi/common/script.js
@@ -17,39 +17,69 @@ const closedrop = () => {
 headerList.onmouseover = opendrop;
 dropDown.onmouseover = opendrop;
 dropDown.onmouseout = closedrop;
+//===============================================================
+// main popup
 
+function openPopup() {
+  window.open(
+    "./popup.html",
+    "popup.html",
+    "width=478,height=578,top=50%,left=50%,status=no,toolbar=no,scrollbars=no,menubar=no,resizable=no"
+  );
+}
+openPopup();
 //===============================================================
 // footer popup
 
-const popup2 = document.querySelector("#popup2");
-
-const popupWidth2 = 600;
-const popupheight2 = 500;
-
-popup2.addEventListener("click", () => {
-  const left = (window.screen.availWidth - popupWidth2) / 2;
-  const top = (window.screen.availHeight - popupheight2) / 2;
-  window.open(
-    "popup2.html",
-    "event",
-    `width=${popupWidth2} height =${popupheight2} left =${left} top=${top}`
-  );
-});
 const popup3 = document.querySelector("#popup3");
-
-const popupWidth3 = 600;
-const popupheight3 = 500;
+const popup3Pop = document.querySelector(".popup3");
+const popup3Close = document.querySelector("#popup3-close");
 
 popup3.addEventListener("click", () => {
-  const left = (window.screen.availWidth - popupWidth3) / 2;
-  const top = (window.screen.availHeight - popupheight3) / 2;
-  window.open(
-    "popup3.html",
-    "event",
-    `width=${popupWidth3} height =${popupheight3} left =${left} top=${top}`
-  );
+  popup3Pop.style.display = "inline-block";
+});
+popup3Close.addEventListener("click", () => {
+  popup3Pop.style.display = "none";
 });
 
+const popup2 = document.querySelector("#popup2");
+const popup2Pop = document.querySelector(".popup2");
+const popup2Close = document.querySelector("#popup2-close");
+
+popup2.addEventListener("click", () => {
+  popup2Pop.style.display = "inline-block";
+});
+popup2Close.addEventListener("click", () => {
+  popup2Pop.style.display = "none";
+});
+// const popup2 = document.querySelector("#popup2");
+
+// const popupWidth2 = 600;
+// const popupheight2 = 500;
+
+// // popup2.addEventListener("click", () => {
+// //   const left = (window.screen.availWidth - popupWidth2) / 2;
+// //   const top = (window.screen.availHeight - popupheight2) / 2;
+// //   window.open(
+// //     "popup2.html",
+// //     "event",
+// //     `width=${popupWidth2} height =${popupheight2} left =${left} top=${top}`
+// //   );
+// // });
+// const popup3 = document.querySelector("#popup3");
+
+// const popupWidth3 = 600;
+// const popupheight3 = 500;
+
+// // popup3.addEventListener("click", () => {
+// //   const left = (window.screen.availWidth - popupWidth3) / 2;
+// //   const top = (window.screen.availHeight - popupheight3) / 2;
+// //   window.open(
+// //     "popup3.html",
+// //     "event",
+// //     `width=${popupWidth3} height =${popupheight3} left =${left} top=${top}`
+// //   );
+// // });
 
 //===============================================================
 // aside
@@ -85,11 +115,10 @@ close.addEventListener("click", () => {
 const depth3s = document.querySelectorAll(".list-depth-3");
 
 depth3s.forEach((depth3) => {
-  depth3.addEventListener("click", ()=> {
-    depth3.classList.toggle("depth3clicked")
-  })
+  depth3.addEventListener("click", () => {
+    depth3.classList.toggle("depth3clicked");
+  });
 });
-
 
 //=================================
 //slider
@@ -101,12 +130,16 @@ const src = document.createAttribute("src");
 const slidePagers = document.querySelector(".slide_pagers");
 const pagers = document.querySelectorAll(".slide_pagers li");
 
-const imgs = ["res-banner01.jpg", "res-banner02.jpg", "res-banner03.jpg", "res-banner04.jpg"];
+const imgs = [
+  "res-banner01.jpg",
+  "res-banner02.jpg",
+  "res-banner03.jpg",
+  "res-banner04.jpg",
+];
 
 const imgSrc = `../img/01main/main/topsection/${imgs[0]}`;
 
 src.value = imgSrc;
-
 img.setAttributeNode(src);
 imgContainer.appendChild(img);
 
@@ -155,19 +188,19 @@ const reset = () => {
   });
 };
 
-imgContainer.addEventListener("mouseenter", () => {
-  stopSlied();
-});
-slidePagers.addEventListener("mouseenter", () => {
-  stopSlied();
-});
+// imgContainer.addEventListener("mouseenter", () => {
+//   stopSlied();
+// });
+// slidePagers.addEventListener("mouseenter", () => {
+//   stopSlied();
+// });
 
-imgContainer.addEventListener("mouseleave", () => {
-  autoSlide();
-});
-slidePagers.addEventListener("mouseleave", () => {
-  autoSlide();
-});
+// imgContainer.addEventListener("mouseleave", () => {
+//   autoSlide();
+// });
+// slidePagers.addEventListener("mouseleave", () => {
+//   autoSlide();
+// });
 
 const pagerChange = (e) => {
   const target = e.target.dataset.index;
