13454048006bed71667f6dd60f5b24413624582d
jbinyim <jbinyim12@naver.com>
2024-03-19 02:38:23 +0900

    jb press

--- theventi/community/press/press.js
+++ theventi/community/press/press.js
@@ -1,9 +1,102 @@
-const countPages = 12;
+const countPages = 10;
+const numberPage = document.querySelector(".page");
+const tbody = document.querySelector(".press-tbody");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+let pageNumberBtns;
+let currentPage = 1;
+
+const getTotalPages = (press) => {
+  return Math.ceil(press.length / countPages);
+};
+
+const setPageBtn = (press) => {
+  numberPage.innerHTML = "";
+  for (let i = 1; i <= getTotalPages(press); i++) {
+    numberPage.innerHTML += `<span class="number-button">${i}</span>`;
+  }
+  numberPage.firstChild.classList.add("selected");
+  pageNumberBtns = document.querySelectorAll(".number-button");
+};
+
+const moveselectPage = () => {
+  const pageBtns = document.querySelectorAll(".number-button");
+
+  pageBtns.forEach((pageBtn) => {
+    if (pageBtn.classList.contains("selected")) {
+      pageBtn.classList.remove("selected");
+    }
+  });
+  pageBtns[currentPage - 1].classList.add("selected");
+};
 
 fetch("press.json")
   .then((response) => response.json())
-  .then((data) => {});
+  .then((press) => {
+    getTotalPages(press);
+    // console.log(getTotalPages());
+    setPageBtn(press);
 
-const getTotalPages = () => {
-  return Math.ceil();
-};
+    const setPageOf = (pageNumber) => {
+      tbody.innerHTML = "";
+
+      for (
+        let i = countPages * (pageNumber - 1) + 1;
+        i <= countPages * (pageNumber - 1) + 10 && i <= press.length;
+        i++
+      ) {
+        const tr = document.createElement("tr");
+        tr.className = "press-tr";
+
+        const pressNumber = document.createElement("td");
+        pressNumber.className = "press-td number";
+
+        const pressTitle = document.createElement("td");
+        pressTitle.className = "press-td title";
+
+        const pressLink = document.createElement("a");
+        pressLink.className = "press-link";
+
+        const pressVenti = document.createElement("td");
+        pressVenti.className = "press-td venti";
+
+        const pressDay = document.createElement("td");
+        pressDay.className = "press-td day";
+
+        pressNumber.innerHTML = 199 - i;
+        pressLink.innerHTML = press[i - 1].title;
+        pressVenti.innerText = "더벤티";
+        pressDay.innerHTML = press[i - 1].day;
+
+        pressTitle.appendChild(pressLink);
+        tr.append(pressNumber, pressTitle, pressVenti, pressDay);
+        tbody.appendChild(tr);
+      }
+    };
+
+    setPageOf(currentPage);
+
+    pageNumberBtns.forEach((numberBtn) => {
+      numberBtn.addEventListener("click", (e) => {
+        currentPage = e.target.innerHTML;
+        setPageOf(currentPage);
+        moveselectPage();
+      });
+    });
+
+    prevBtn.addEventListener("click", () => {
+      if (currentPage > 1) {
+        currentPage -= 1;
+        setPageOf(currentPage);
+        moveselectPage();
+      }
+    });
+    nextBtn.addEventListener("click", () => {
+      if (currentPage < pageNumberBtns.length) {
+        currentPage += 1;
+        setPageOf(currentPage);
+        moveselectPage();
+      }
+    });
+  });
